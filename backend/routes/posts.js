fe0518d6a76f3ad973ca978c12e7f777f555940d
dmkucheryavenko <dmytro.kucheryavenko@eliftech.com>
2017-02-01 19:10:00 +0200

    add router for post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,19 +4,30 @@ const basePath = '/users/:userId';
 
 module.exports = function (app) {
     app.get(`${basePath}/posts`, function (req, res) {
-        res.send('hello world');
+        models.Post.findAll({UserId: req.params.userId}).then((posts) => {
+            res.json(posts);
+        });
     });
     app.post(`${basePath}/posts`, function (req, res) {
-        models.User.create({username: 'John'});
-        res.send('hello world');
+        models.Post.create(Object.assign(req.body, {UserId: req.params.id})).then((post) => {
+            res.json(post);
+        });
     });
     app.get(`${basePath}/posts/:id`, function (req, res) {
-        res.send('hello world');
+        models.Post.findById(req.params.id).then((post) => {
+            res.json(user);
+        });
     });
     app.put(`${basePath}/posts/:id`, function (req, res) {
-        res.send('hello world');
+        models.Post.update(req.body, {where: {id: req.params.id}, returning: false}).then(() => {
+            return models.Post.findById(req.params.id)
+        }).then((post) => {
+            res.json(post);
+        });
     });
     app.delete(`${basePath}/posts/:id`, function (req, res) {
-        res.send('hello world');
+        models.Post.destroy({where: {id: req.params.id}}).then((users) => {
+            res.sendStatus(200);
+        });
     });
 };
