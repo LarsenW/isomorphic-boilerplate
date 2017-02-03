70b98e318618b1439d663a9c09027cd35cbffdfc
dmkucheryavenko <dmytro.kucheryavenko@eliftech.com>
2017-02-03 17:25:53 +0200

    add react router

---
+++ frontend/src/components/CounterPage/CounterPage.jsx
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+
+class CounterPage extends Component {
+    render() {
+        return <div>Заглушка для счетчиков</div>;
+    }
+}
+
+export default CounterPage;
